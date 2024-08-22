98138ba76f778362d6a5fc10c7ed4dcef323b122
sebastianpiceda <sebastian.piceda@fing.edu.uy>
2024-08-22 17:26:17 -0300

    agrego manejador de claves quemadas (las claves quemadas se guardan en texto plano)

--- src/pam_auth_casero.c
+++ src/pam_auth_casero.c
@@ -9,36 +9,19 @@
 #include <pwd.h>
 #define ARCHIVO_SEMILLA ".seed_auth_casero"
 
-
 /* Función para leer la semilla de autenticación de un archivo.
  Retorna la semilla leída o NULL en caso de error.
  La semilla debe tener 32 caracteres. El archivo debe estar en el directorio home del usuario, 
  y llamarse '.seed_auth_casero'.
  El archivo debe contener la semilla en la primera línea.
 */
-const char *leer_semilla_de_archivo(pam_handle_t *pamh) 
+const char *leer_semilla_de_archivo(const char *dir_home) 
 {
     static char semilla[33]; // 32 caracteres + 1 para el terminador nulo
     char ruta[512];
-    const char *user;
-    struct passwd *pw;
-
-    // Obtener el nombre de usuario
-    if (pam_get_user(pamh, &user, NULL) != PAM_SUCCESS || user == NULL) {
-        syslog(LOG_ERR, "No se pudo obtener el nombre de usuario");
-        return NULL; // Error al obtener el nombre de usuario
-
-    }
-
-    // Obtener la estructura passwd del usuario
-    pw = getpwnam(user);
-    if (pw == NULL) {
-        syslog(LOG_ERR, "No se pudo obtener la información del usuario");
-        return NULL; // Error al obtener la información del usuario
-    }
 
     // Se construye la ruta del archivo de autenticación
-    snprintf(ruta, sizeof(ruta), "%s/%s", pw->pw_dir, ARCHIVO_SEMILLA);
+    snprintf(ruta, sizeof(ruta), "%s/%s", dir_home, ARCHIVO_SEMILLA);
     syslog(LOG_INFO, "Ruta para buscar la semilla: %s", ruta); // debug
 
     FILE *archivo = fopen(ruta, "r");
@@ -81,7 +64,6 @@ int chequear_totp(const char *semilla, const char *totp_usuario)
         syslog(LOG_ERR, "Error al generar el TOTP");
         return 0;
     }
-    syslog(LOG_ERR, "No se pudo leer la semilla de autenticación");
     
     int resultado = strcmp(totp_generado, totp_usuario) == 0;
     free(totp_generado); // Asegúrate de liberar la memoria
@@ -89,17 +71,73 @@ int chequear_totp(const char *semilla, const char *totp_usuario)
     return resultado;
 }
 
+bool verificar_usado(const char *totp_usuario, const char *dir_home){
+    char ruta[512];
+
+    // Se construye la ruta del archivo de autenticación
+    snprintf(ruta, sizeof(ruta), "%s/%s", dir_home, ".tokens_usados.txt");
+    syslog(LOG_INFO, "Ruta para buscar la semilla: %s", ruta); // debug
+    FILE *tokens_usados = fopen(ruta, "r");
+    if (tokens_usados == NULL) {
+        return false; // Si no se puede abrir el archivo, asumimos que el token no ha sido usado
+    }
+
+    char token[7];
+    while(fgets(token, sizeof(token), tokens_usados) != NULL){
+        // Eliminar el salto de línea al final del token
+        int largo = strlen(token);
+        if (largo > 0 && token[largo - 1] == '\n') {
+            token[largo - 1] = '\0';
+        }
+        if(strcmp(token, totp_usuario) == 0){
+            fclose(tokens_usados);
+            return true;
+        }
+    }
+    fclose(tokens_usados);
+    return false;
+}
+
+void agregar_token_usado(const char *totp_usuario, const char *dir_home){
+    char ruta[512];
+
+    // Se construye la ruta del archivo de autenticación
+    snprintf(ruta, sizeof(ruta), "%s/%s", dir_home, ".tokens_usados.txt");
+    syslog(LOG_INFO, "Ruta para buscar la semilla: %s", ruta); // debug
+    FILE *tokens_usados = fopen(ruta, "a");
+    if (tokens_usados != NULL) {
+        fprintf(tokens_usados, "%s\n", totp_usuario);
+        fclose(tokens_usados);
+    }
+}
+
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+    const char *user;
+    struct passwd *pw;
     int retorno;
     char *totp_usuario = NULL;
 
     // Inicio el log
     openlog("pam_aut_casero", LOG_PID | LOG_CONS, LOG_AUTH);
 
+    // Obtener el nombre de usuario
+    if (pam_get_user(pamh, &user, NULL) != PAM_SUCCESS || user == NULL) {
+        syslog(LOG_ERR, "No se pudo obtener el nombre de usuario");
+        closelog();
+        return PAM_AUTH_ERR; // Error al obtener el nombre de usuario
+    }
+
+    // Obtener la estructura passwd del usuario
+    pw = getpwnam(user);
+    if (pw == NULL) {
+        syslog(LOG_ERR, "No se pudo obtener la información del usuario");
+        closelog();
+        return PAM_AUTH_ERR; // Error al obtener la información del usuario
+    }
+    const char *dir_home = pw->pw_dir;
+
     // Solicitar el código TOTP al usuario
     retorno = pam_prompt(pamh, PAM_PROMPT_ECHO_OFF, &totp_usuario, "Ingrese el código TOTP: ");
-    
-
     if (retorno != PAM_SUCCESS || totp_usuario == NULL) {
         syslog(LOG_ERR, "Error al obtener el código TOTP del usuario: %d", retorno);
         closelog();
@@ -107,9 +145,16 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, cons
         return PAM_AUTH_ERR;
     }
 
-    // Leer la semilla de autenticación del archivo
-    const char *semilla = leer_semilla_de_archivo(pamh);
+    // Verificar si el token ya ha sido usado
+    if (verificar_usado(totp_usuario, dir_home)) {
+        syslog(LOG_ERR, "El token ya fue usado");
+        closelog();
+        free(totp_usuario);
+        return PAM_AUTH_ERR;
+    }
 
+    // Leer la semilla de autenticación del archivo
+    const char *semilla = leer_semilla_de_archivo(dir_home);
     if (semilla == NULL) {
         syslog(LOG_ERR, "No se pudo leer la semilla de autenticación");
         closelog();
@@ -120,13 +165,14 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, cons
     // Verificar el código TOTP
     if (chequear_totp(semilla, totp_usuario)) {
         syslog(LOG_INFO, "TOTP válido, acceso permitido");
-        closelog();
+        agregar_token_usado(totp_usuario, dir_home);
         free(totp_usuario);
+        closelog();
         return PAM_SUCCESS; // TOTP es válido
     } else {
         syslog(LOG_INFO, "TOTP inválido, acceso denegado");
-        closelog();
         free(totp_usuario);
+        closelog();
         return PAM_AUTH_ERR; // TOTP inválido
     }
 }
